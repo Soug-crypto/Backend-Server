96bbff61843c712ff226e5d66c695ecb54a14024
Ahmed Wheida <ahmed.wheida@gmail.com>
2020-01-13 10:58:35 +0100

    added the server-server connection

--- routes/messages.js
+++ routes/messages.js
@@ -3,13 +3,14 @@ var router = express.Router();
 const MessagesModel = require("../models/Messages");
 const validators = require('../userInputValidators/validators');
 const io = require('../bin/www');
+const axios = require ('axios');
+
 
 router.get("/", (req, res, next) => {
     res.send("respond with a message");
 });
 
 router.post("/store", validators['messagesStoreValidatorArray'], validators['validatorfunction'], (req, res) => {
-    // Todo fix validation problem
     let {content, user_id, bubble_id} = req.body;
 
     MessagesModel.store(
@@ -19,6 +20,11 @@ router.post("/store", validators['messagesStoreValidatorArray'], validators['val
     ).then(message => {
         // Get the IO object and send message to all users in the bubble
         io.getIO().sockets.to(bubble_id).emit('message', message);
+        axios({'POST',
+            url: `http://localhost:8000/`,
+            headers,
+            data: {id:user_id, message: content}
+        });
         res.sendStatus(201);
     }).catch(err => {
         console.log({err});
